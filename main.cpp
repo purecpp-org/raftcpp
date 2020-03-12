efb0a8b7c75dd0535138dca9de4c2690cd2714b2
qicosmos <qicosmos@163.com>
2020-03-12 10:37:19 +0800

    modify text edit format.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <include/rpc_server.h>
-using namespace rest_rpc;
-using namespace rest_rpc::rpc_service;
 
 int main() {
-	rpc_server server(9000, std::thread::hardware_concurrency());
+    using namespace rest_rpc;
+    using namespace rest_rpc::rpc_service;
 
-	server.register_handler("hello", [](rpc_conn conn) {
-		return "hello world!";
-	});
+    rpc_server server(9000, std::thread::hardware_concurrency());
 
-	server.run();
-}
+    server.register_handler("hello", [](rpc_conn conn) {
+        return "hello world!";
+    });
+
+    server.run();
+} 
