2733b791b9b88e5d33209f2daef13919e6b29004
20083017 <651756340@qq.com>
2020-08-02 22:42:06 +0800

    change version

--- cmake/Modules/Rest_rpcExternalProject.cmake
+++ cmake/Modules/Rest_rpcExternalProject.cmake
@@ -6,7 +6,7 @@
 #  - REST_RPC_INCLUDE_DIR
 #  - MSGPACK_INCLUDE_DIR
 
-#set(REST_RPC_VERSION "1.8.0")
+set(REST_RPC_VERSION "0.05")
 
 set(REST_RPC_PREFIX "${CMAKE_CURRENT_BINARY_DIR}/external/rest-rpc-install")
 set(REST_RPC_INCLUDE_DIR "${REST_RPC_PREFIX}/include")
@@ -16,7 +16,7 @@ set(REST_RPC_URL_MD5 "d638bb0659b65161c3b8ab8c6e190ea0")
 
 ExternalProject_Add(rest_rpc_ep
         PREFIX external/rest-rpc
-        URL "https://github.com/qicosmos/rest_rpc/archive/V0.05.tar.gz"
+        URL "https://github.com/qicosmos/rest_rpc/archive/V${REST_RPC_VERSION}.tar.gz"
         URL_MD5 ${REST_RPC_URL_MD5}
         BUILD_IN_SOURCE 1
         CONFIGURE_COMMAND bash -c "git submodule init && git submodule update"
