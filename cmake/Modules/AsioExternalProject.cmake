0be7a185ec2a1a607d806d048bd479ab7d89fde9
Qing Wang <kingchin1218@126.com>
2020-08-02 17:26:24 +0800

    FIX

--- cmake/Modules/AsioExternalProject.cmake
+++ cmake/Modules/AsioExternalProject.cmake
@@ -6,32 +6,12 @@
 #  - ASIO_INCLUDE_DIR
 
 set(ASIO_VERSION "1.16.1")
-
-if(APPLE)
-    set(ASIO_CMAKE_CXX_FLAGS "-fPIC -DASIO_USE_OWN_TR1_TUPLE=1 -Wno-unused-value -Wno-ignored-attributes")
-elseif(NOT MSVC)
-    set(ASIO_CMAKE_CXX_FLAGS "-fPIC")
-endif()
-
-if(CMAKE_BUILD_TYPE)
-    string(TOUPPER ${CMAKE_BUILD_TYPE} UPPERCASE_BUILD_TYPE)
-endif()
-
-set(ASIO_CMAKE_CXX_FLAGS "${EP_CXX_FLAGS} ${CMAKE_CXX_FLAGS_${UPPERCASE_BUILD_TYPE}} ${ASIO_CMAKE_CXX_FLAGS}")
-
 set(ASIO_PREFIX "${CMAKE_CURRENT_BINARY_DIR}/external/asio-install")
 set(ASIO_INCLUDE_DIR "${ASIO_PREFIX}")
 
-
-set(ASIO_CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-        -DCMAKE_INSTALL_PREFIX=${ASIO_PREFIX}
-        -DCMAKE_CXX_FLAGS=${ASIO_CMAKE_CXX_FLAGS})
-
-set(ASIO_URL_MD5 "d93a188b2d93a06d9acdb1484287c071")
+set(ASIO_URL_MD5 "7f82f1c5c8dc048b634ab44a4594d341")
 
 ExternalProject_Add(asio_ep
         PREFIX external/asio
-        URL "https://sourceforge.net/projects/asio/files/asio/${GTEST_VERSION}%20%28Stable%29/boost_asio_1_16_1.tar.gz"
-        URL_MD5 ${ASIO_URL_MD5}
-        CMAKE_ARGS ${ASIO_CMAKE_ARGS}
-        ${EP_LOG_OPTIONS})
+        URL "https://github.com/chriskohlhoff/asio/archive/asio-1-16-1.tar.gz"
+        URL_MD5 ${ASIO_URL_MD5})
--- cmake/Modules/Rest_rpcExternalProject.cmake
+++ cmake/Modules/Rest_rpcExternalProject.cmake
@@ -8,31 +8,13 @@
 
 #set(REST_RPC_VERSION "1.8.0")
 
-if(APPLE)
-    set(REST_RPC_CMAKE_CXX_FLAGS "-fPIC -DREST_RPC_USE_OWN_TR1_TUPLE=1 -Wno-unused-value -Wno-ignored-attributes")
-elseif(NOT MSVC)
-    set(REST_RPC_CMAKE_CXX_FLAGS "-fPIC")
-endif()
-
-if(CMAKE_BUILD_TYPE)
-    string(TOUPPER ${CMAKE_BUILD_TYPE} UPPERCASE_BUILD_TYPE)
-endif()
-
-set(REST_RPC_CMAKE_CXX_FLAGS "${EP_CXX_FLAGS} ${CMAKE_CXX_FLAGS_${UPPERCASE_BUILD_TYPE}} ${REST_RPC_CMAKE_CXX_FLAGS}")
-
 set(REST_RPC_PREFIX "${CMAKE_CURRENT_BINARY_DIR}/external/rest-rpc-install")
 set(REST_RPC_INCLUDE_DIR "${REST_RPC_PREFIX}/include")
 set(MSGPACK_INCLUDE_DIR "${REST_RPC_PREFIX}/third/msgpack/include")
-
-set(REST_RPC_CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-        -DCMAKE_INSTALL_PREFIX=${REST_RPC_PREFIX}
-        -DCMAKE_CXX_FLAGS=${REST_RPC_CMAKE_CXX_FLAGS})
 
 set(REST_RPC_URL_MD5 "bb35e65ccb4928bdb7496d81840195bf")
 
 ExternalProject_Add(rest_rpc_ep
         PREFIX external/rest-rpc
         URL "https://github.com/qicosmos/rest_rpc/archive/master.zip"
-        URL_MD5 ${REST_RPC_URL_MD5}
-        CMAKE_ARGS ${REST_RPC_CMAKE_ARGS}
-        ${EP_LOG_OPTIONS})
+        URL_MD5 ${REST_RPC_URL_MD5})
