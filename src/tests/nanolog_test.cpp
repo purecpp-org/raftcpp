9c868e68abbab509263d2db47d73c390e8e8d723
BryainZhang <pgdevuser@163.com>
2020-08-05 00:02:02 +0800

    Use doctest instead of gtest. (#32)
    * 。
    
    * add CI to support centos7
    
    * add CI to support centos7
    
    * add CI to support centos7
    
    * add CI to support macos&ubuntu
    
    * replace gtest to doctest
    
    * replace gtest with doctest
    
    * replace gtest to doctest
    
    * replace gtest with doctest
    
    * replace gtest with doctest

--- src/tests/nanolog_test.cpp
+++ src/tests/nanolog_test.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 #include <string>
-#include "gtest/gtest.h"
 #include "nanolog.hpp"
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
 
-class TestNanoLog : public testing::Test {
+//nanolog INFO (WARN) and doctest INFO (WARN) conflict
+#ifdef INFO
+#undef INFO
+#endif
+
+#ifdef WARN
+#undef WARN
+#endif
+
+class TestNanoLog {
 public:
     static void SetUpTestCase() {
         std::cout << "TestNanoLog SetUpTestCase" << std::endl;
@@ -14,7 +24,7 @@ public:
     }
 };
 
-TEST_F(TestNanoLog, TestLogLevel) {
+TEST_CASE_FIXTURE(TestNanoLog, "TestLogLevel") {
     std::string log_directory = "/tmp/";
     std::string log_file_name = "test_nanolog";
     nanolog::initialize(nanolog::GuaranteedLogger(), log_directory, log_file_name, 1);
@@ -36,7 +46,7 @@ TEST_F(TestNanoLog, TestLogLevel) {
     nanolog::initialize(nanolog::GuaranteedLogger(), "", "", 1);
 
     std::fstream file(real_log_file_path);
-    ASSERT_TRUE(file);
+    REQUIRE(file);
 
     std::string line;
     uint8_t debug_count=0;
@@ -55,15 +65,11 @@ TEST_F(TestNanoLog, TestLogLevel) {
         }
     }
 
-    ASSERT_EQ(debug_count, 6);
-    ASSERT_EQ(info_count, 6);
-    ASSERT_EQ(warn_count, 6);
-    ASSERT_EQ(crit_count, 6);
+    REQUIRE_EQ(debug_count, 6);
+    REQUIRE_EQ(info_count, 6);
+    REQUIRE_EQ(warn_count, 6);
+    REQUIRE_EQ(crit_count, 6);
 
     std::remove(real_log_file_path.data());
 }
 
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
