7343fc0bee65e90ffa9d012e9edb919813cb87f4
Qing Wang <kingchin1218@gmail.com>
2020-08-21 00:10:13 +0800

    Add more test for flags

--- tests/flag_test.cc
+++ tests/flag_test.cc
@@ -7,8 +7,10 @@
 
 TEST_CASE("flags_test") {
     using namespace raftcpp::common;
-    char *argv[] = {"MyTestApp", "--addr", "localhost"};
-    Flags flags(3, argv);
+    char *argv[] = {"MyTestApp", "--addr", "localhost", "--port", "8888"};
+    Flags flags(5, argv);
     const auto addr = flags.FlagStr("addr");
+    const auto port = flags.FlagInt("port");
     REQUIRE_EQ("localhost", addr);
+    REQUIRE_EQ(8888, port);
 }
