20814a227196b2ce9032a3366c5e2bf392b515d7
Qing Wang <kingchin1218@gmail.com>
2020-08-21 13:28:02 +0800

    Refine run_all_tests for ci (#48)

---
+++ run_all_tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+#set -x
+
+for file in `ls`; do
+    if [ "test_" = "${file:0:5}" ];then
+        echo "===========================RUNNING test $file==============================="
+        ("./$file")
+        echo "===========================PASSED  test $file==============================="
+        echo ""
+        echo ""
+        echo ""
+    fi
+done
