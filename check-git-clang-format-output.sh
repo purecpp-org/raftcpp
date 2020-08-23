6edec22fa4dcdebe6943304569f0c603e58798f6
wxing2008666 <wxing2008666@126.com>
2020-08-23 13:51:13 +0800

    modify git-clang-format

--- check-git-clang-format-output.sh
+++ check-git-clang-format-output.sh
@@ -13,7 +13,7 @@ else
   origin_commit=4b825dc642cb6eb9a060e54bf8d69288fbee4904
   echo "Running clang-format against branch $base_commit, with hash $(git rev-parse "$base_commit")"
 fi
-output="$(./git-clang-format --binary /usr/bin/clang-format --commit "$base_commit" "$origin_commit" --diff)"
+output="$(git-clang-format --binary clang-format --commit "$base_commit" "$origin_commit" --diff)"
 if [ "$output" = "no modified files to format" ] || [ "$output" = "clang-format did not modify any files" ] ; then
   echo "clang-format passed."
   exit 0
