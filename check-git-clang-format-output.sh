0b9b872d4ef9b1f2e780b4945cdb2886b4a915e3
wxing2008666 <wxing2008666@126.com>
2020-08-23 21:17:47 +0800

    modify git-clang-format sh

--- check-git-clang-format-output.sh
+++ check-git-clang-format-output.sh
@@ -6,14 +6,14 @@ set -x
 if git rev-parse --verify HEAD >/dev/null 2>&1; then
   # Not in a pull request, so compare against parent commit
   base_commit=`git rev-parse --verify HEAD`
-  origin_commit=`git rev-parse --verify origin/HEAD`
+  origin_commit=`git rev-parse --verify remotes/origin/HEAD`
   echo "Running clang-format against parent commit $base_commit, and $origin_commit"
 else
   base_commit=4b825dc642cb6eb9a060e54bf8d69288fbee4904
   origin_commit=4b825dc642cb6eb9a060e54bf8d69288fbee4904
   echo "Running clang-format against branch $base_commit, with hash $(git rev-parse "$base_commit")"
 fi
-output="$(git-clang-format --binary clang-format --commit "$base_commit" "$origin_commit" --diff)"
+output="$(./git-clang-format --binary /usr/bin/clang-format --commit "$base_commit" "$origin_commit" --diff)"
 if [ "$output" = "no modified files to format" ] || [ "$output" = "clang-format did not modify any files" ] ; then
   echo "clang-format passed."
   exit 0
