e3d6de511f5d4632bd81bbf50f9eb0bb611174e9
wxing2008666 <wxing2008666@126.com>
2020-08-24 11:07:28 +0800

    dev test

--- check-git-clang-format-output.sh
+++ check-git-clang-format-output.sh
@@ -8,6 +8,7 @@ if git rev-parse --verify HEAD >/dev/null 2>&1; then
   base_commit=`git rev-parse --verify HEAD`
   echo "$(git rev-parse --git-dir)"
   builtin cd "$(git rev-parse --git-dir)"
+  builtin cd refs/heads/
   ls
   git checkout --progress --force refs/remotes/origin/master >/dev/null 2>&1
   origin_commit=`git rev-parse --verify refs/remotes/origin/master`
