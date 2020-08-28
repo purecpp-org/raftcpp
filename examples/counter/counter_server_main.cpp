426005ff05b42672081ab1ee02d70ab4c1f70da0
Qing Wang <kingchin1218@gmail.com>
2020-08-28 23:08:47 +0800

    Fix lint

--- examples/counter/counter_server_main.cpp
+++ examples/counter/counter_server_main.cpp
@@ -1,3 +1,5 @@
+#include <gflags/gflags.h>
+
 #include <thread>
 
 #include "common/type_def.h"
@@ -5,16 +7,14 @@
 #include "node/node.h"
 #include "rpc_server.h"
 
-#include <gflags/gflags.h>
-
 using namespace rest_rpc;
 using namespace rpc_service;
 
 using namespace examples;
 using namespace examples::counter;
 
-//DEFINE_string(raftcpp_conf, "", "The configurations of this raft group.");
-//DEFINE_string(this_addr, "", "This address of this instance listening on.");
+// DEFINE_string(raftcpp_conf, "", "The configurations of this raft group.");
+// DEFINE_string(this_addr, "", "This address of this instance listening on.");
 
 class CounterServiceImpl {
     public:
