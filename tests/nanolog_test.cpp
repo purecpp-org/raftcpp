f3023522ccaa7f60b46ccd80006c9a2cd6ec755d
Qing Wang <kingchin1218@gmail.com>
2020-08-28 22:35:47 +0800

    Fix lint

--- tests/nanolog_test.cpp
+++ tests/nanolog_test.cpp
@@ -1,10 +1,11 @@
+#include "nanolog.hpp"
+
 #include <iostream>
 #include <string>
-#include "nanolog.hpp"
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 
-//nanolog INFO (WARN) and doctest INFO (WARN) conflict
+// nanolog INFO (WARN) and doctest INFO (WARN) conflict
 #ifdef INFO
 #undef INFO
 #endif
@@ -14,10 +15,8 @@
 #endif
 
 class TestNanoLog {
-public:
-    static void SetUpTestCase() {
-        std::cout << "TestNanoLog SetUpTestCase" << std::endl;
-    }
+    public:
+    static void SetUpTestCase() { std::cout << "TestNanoLog SetUpTestCase" << std::endl; }
 
     static void TearDownTestCase() {
         std::cout << "TestNanoLog TearDownTestCase" << std::endl;
@@ -26,10 +25,10 @@ public:
 
 TEST_CASE_FIXTURE(TestNanoLog, "TestLogLevel") {
 #ifdef _WIN32
-	//define something for Windows (32-bit and 64-bit, this part is common)
+    // define something for Windows (32-bit and 64-bit, this part is common)
     std::string log_directory = "";
 #else __linux__
-	std::string log_directory = "/tmp/";
+    std::string log_directory = "/tmp/";
 #endif
 
     std::string log_file_name = "test_nanolog";
@@ -48,31 +47,32 @@ TEST_CASE_FIXTURE(TestNanoLog, "TestLogLevel") {
     real_log_file_path.append(std::to_string(++m_file_number));
     real_log_file_path.append(".txt");
 
-    //reset nanologger to call NanoLogger::pop, so that all the log can be written to file.
+    // reset nanologger to call NanoLogger::pop, so that all the log can be written to
+    // file.
     nanolog::initialize(nanolog::GuaranteedLogger(), "", "", 1);
 
     std::fstream file(real_log_file_path);
     REQUIRE(file);
 
     std::string line;
-    uint8_t debug_count=0;
+    uint8_t debug_count = 0;
     uint8_t info_count = 0;
     uint8_t warn_count = 0;
     uint8_t crit_count = 0;
     while (getline(file, line)) {
         if (line.find("DEBUG") != std::string::npos) {
 #ifdef _WIN32
-			//define something for Windows (32-bit and 64-bit, this part is common)
+            // define something for Windows (32-bit and 64-bit, this part is common)
 #ifdef _DEBUG
-			debug_count++;
+            debug_count++;
 #else
-			//debug_count++;
+            // debug_count++;
 #endif
 #else __linux__
 #ifndef NDEBUG
-			//debug_count++;
+            // debug_count++;
 #else
-			debug_count++;
+            debug_count++;
 #endif
 #endif
         } else if (line.find("INFO") != std::string::npos) {
@@ -85,20 +85,19 @@ TEST_CASE_FIXTURE(TestNanoLog, "TestLogLevel") {
     }
 
 #ifdef _WIN32
-	//define something for Windows (32-bit and 64-bit, this part is common)
+    // define something for Windows (32-bit and 64-bit, this part is common)
 #ifdef _DEBUG
-	REQUIRE_EQ(debug_count, 6);
+    REQUIRE_EQ(debug_count, 6);
 #else
-	REQUIRE_EQ(debug_count, 0);
+    REQUIRE_EQ(debug_count, 0);
 #endif
 #else __linux__
 #ifndef NDEBUG
-	REQUIRE_EQ(debug_count, 0);
+    REQUIRE_EQ(debug_count, 0);
 #else
-	REQUIRE_EQ(debug_count, 6);
+    REQUIRE_EQ(debug_count, 6);
 #endif
 #endif
-
 
     REQUIRE_EQ(info_count, 6);
     REQUIRE_EQ(warn_count, 6);
@@ -106,4 +105,3 @@ TEST_CASE_FIXTURE(TestNanoLog, "TestLogLevel") {
 
     std::remove(real_log_file_path.data());
 }
-
