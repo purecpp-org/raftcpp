04e5f19b037a5646e1c6706f49a599680eae2bf9
Qing Wang <kingchin1218@gmail.com>
2020-08-30 23:46:56 +0800

    Add a random timer encapsulation for boost::timer. (#86)
    * Add a timer.
    
    * Add timer test.
    
    * Fix lint
    
    * Fix
    
    * Fix lint
    
    * FIx
    
    * Fix test in macos
    
    * Fix

---
+++ src/common/range.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <string>
+
+namespace raftcpp {
+namespace common {
+
+class Range final {
+    public:
+    Range(uint64_t begin, uint64_t end)
+        : begin_(begin), end_(end), cache_delta_(end_ - begin_) {
+        // TODO(qwang): We should assert `begin <= end`;
+    }
+
+    Range(const Range &other) : Range(other.begin_, other.end_) {}
+
+    uint64_t GetBegin() const { return begin_; }
+
+    uint64_t GetEnd() const { return end_; }
+
+    uint64_t GetDelta() const { return cache_delta_; }
+
+    private:
+    uint64_t begin_;
+
+    uint64_t end_;
+
+    uint64_t cache_delta_;
+};
+
+}  // namespace common
+}  // namespace raftcpp
---
+++ tests/timer_test.cc
@@ -0,0 +1,28 @@
+
+#include "common/timer.h"
+
+#include <asio/io_service.hpp>
+#include <iostream>
+#include <thread>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest.h>
+
+TEST_CASE("Timer-RandomTimer") {
+    using namespace raftcpp::common;
+
+    uint64_t counter = 0;
+
+    asio::io_service io_service;
+    RandomTimer random_timer(io_service,
+                             /*random_range=*/{990, 1000},
+                             [&counter](const asio::error_code &e) { ++counter; });
+    random_timer.Start();
+    std::thread th([&io_service]() { io_service.run(); });
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(5 * 1000 + 500));
+    // Note that stop a io_service is thread safe.
+    io_service.stop();
+    th.join();
+
+    REQUIRE_EQ(5, counter);
+}
