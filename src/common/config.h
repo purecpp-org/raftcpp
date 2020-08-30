36de7c29ee16a0f146e88d94fe5c999312f43b5a
TianYi <tianyime@qq.com>
2020-08-30 22:56:54 +0800

    Implement Config class (#85)

--- src/common/config.h
+++ src/common/config.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <regex>
 #include <string>
 #include <vector>
 
@@ -9,13 +10,28 @@ namespace raftcpp {
 
 class Config final {
     public:
-    static Config From(const std::string &config_str) { return Config(); }
+    static Config From(const std::string &config_str) {
+        Config config;
+        config.endpoints.clear();
+        std::regex reg(R"((\d{1,3}(\.\d{1,3}){3}:\d+))");
+        std::sregex_iterator pos(config_str.begin(), config_str.end(), reg);
+        decltype(pos) end;
+        for (; pos != end; ++pos) {
+            config.PushBack(Endpoint(pos->str()));
+        }
+        return config;
+    }
 
     Endpoint GetSelfEndpoint() const {}
 
-    std::vector<Endpoint> GetAllEndpoints() const {}
+    std::vector<Endpoint> GetAllEndpoints() const { return endpoints; }
 
     private:
+    Config() = default;
+    Config(const Config &c) = default;
+    void PushBack(const Endpoint &endpoint) { endpoints.push_back(endpoint); }
+
+    std::vector<Endpoint> endpoints;
 };
 
 }  // namespace raftcpp
