80914a68d658eae1551a54b06bea5a8c2250e2ee
Qing Wang <kingchin1218@gmail.com>
2020-08-31 21:07:15 +0800

    WIP

---
+++ src/rpc/services.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <rpc_server.h>
+
+namespace raftcpp {
+namespace rpc {
+
+/**
+ * The rpc service that communicates with other nodes.
+ */
+class NodeService {
+public:
+    void RequestVote(rest_rpc::rpc_service::rpc_conn conn, ) {
+
+    }
+
+private:
+
+};
+
+} // namespace rpc
+} // namespace raftcpp
+
