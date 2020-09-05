6edd630c238f6650d067b120936c7ad15769bfa3
wxing <wxing2008666@126.com>
2020-09-06 01:02:54 +0800

    Add log manager and test (#117)
    * add log manager and test
    
    * modify format
    
    * delete free lock from boost
    
    * code format
    
    * code format
    
    * code format
    
    * code format
    
    * format
    
    * repeated timer and log manager test
    
    * code format

--- src/common/timer.cc
+++ src/common/timer.cc
@@ -7,38 +7,23 @@ namespace common {
 
 void RepeatedTimer::Start(const uint64_t timeout_ms) { Reset(timeout_ms); }
 
-void RepeatedTimer::Stop() {
-    std::lock_guard<std::shared_mutex> guard{shared_mutex_};
-    is_running_ = false;
-}
+void RepeatedTimer::Stop() { is_running_.store(false); }
 
 void RepeatedTimer::Reset(const uint64_t timeout_ms) {
-    {
-        std::lock_guard<std::shared_mutex> guard{shared_mutex_};
-        is_running_ = true;
-    }
+    is_running_.store(true);
     DoSetExpired(timeout_ms);
 }
 
 void RepeatedTimer::DoSetExpired(const uint64_t timeout_ms) {
-    std::shared_lock<std::shared_mutex> guard{shared_mutex_};
-    if (!is_running_) {
+    if (!is_running_.load()) {
         return;
     }
 
     timer_.expires_from_now(std::chrono::milliseconds(timeout_ms));
     timer_.async_wait([this, timeout_ms](const asio::error_code &e) {
-        {
-            if (e.value() == asio::error::operation_aborted) {
-                // The timer was canceled.
-                return;
-            }
-
-            std::shared_lock<std::shared_mutex> guard{shared_mutex_};
-            if (!is_running_) {
-                // The timer is stopping, should we should trigger any handler.
-                return;
-            }
+        if (e.value() == asio::error::operation_aborted || !is_running_.load()) {
+            // The timer was canceled.
+            return;
         }
         timeout_handler_(e);
         this->DoSetExpired(timeout_ms);
--- src/log_manager/log_manager_mutex_impl.h
+++ src/log_manager/log_manager_mutex_impl.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <condition_variable>
 #include <mutex>
 #include <queue>
 
@@ -9,27 +10,48 @@ namespace raftcpp {
 
 template <typename LogEntryType>
 class LogManagerMutexImpl : public LogManagerInterface<LogEntryType> {
-    public:
+public:
     LogManagerMutexImpl() = default;
 
     ~LogManagerMutexImpl() = default;
 
-    LogEntryType Pop() override {
-        std::lock_guard<std::mutex> guard{queue_mutex_};
-        LogEntryType ret;
-        ret = queue_.front();
-        return ret;
-    }
+    virtual LogEntryType Pop() override;
 
-    void Push(const LogEntryType &log_entry) override {
-        std::lock_guard<std::mutex> guard{queue_mutex_};
-        queue_.push(log_entry);
-    }
+    virtual bool Pop(LogEntryType &log_entry) override;
 
-    private:
+    virtual void Push(const LogEntryType &log_entry) override;
+
+private:
     std::mutex queue_mutex_;
-
+    std::condition_variable queue_cv_;
     std::queue<LogEntryType> queue_;
 };
+
+template <typename LogEntryType>
+LogEntryType LogManagerMutexImpl<LogEntryType>::Pop() {
+    std::unique_lock<std::mutex> lock(queue_mutex_);
+    queue_cv_.wait(lock, [this] { return !queue_.empty(); });
+    LogEntryType log_entry_type = queue_.front();
+    queue_.pop();
+    return log_entry_type;
+}
+
+template <typename LogEntryType>
+bool LogManagerMutexImpl<LogEntryType>::Pop(LogEntryType &log_entry) {
+    std::unique_lock<std::mutex> lock(queue_mutex_);
+    if (!queue_.empty()) {
+        log_entry = queue_.front();
+        queue_.pop();
+        return true;
+    }
+    return false;
+}
+
+template <typename LogEntryType>
+void LogManagerMutexImpl<LogEntryType>::Push(const LogEntryType &log_entry) {
+    std::unique_lock<std::mutex> lock(queue_mutex_);
+    queue_.push(log_entry);
+    queue_cv_.notify_all();
+}
 
 }  // namespace raftcpp
