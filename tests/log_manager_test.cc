c738d163fee3dbbb07df8defb4785f876921b161
wxing2008666 <wxing2008666@126.com>
2020-09-06 00:52:11 +0800

    repeated timer and log manager test

--- tests/log_manager_test.cc
+++ tests/log_manager_test.cc
@@ -1,10 +1,14 @@
+#include <thread>
+#include <chrono>
 #include "log_manager/log_manager_mutex_impl.h"
 
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest.h>
 
+using namespace raftcpp;
+
 struct LogEntryTest {
-    LogEntryTest() : index_(0), term_(0) {}
+    LogEntryTest() : index_(1), term_(1) {}
     LogEntryTest(int64_t index, int64_t term) : index_(index), term_(term) {}
     LogEntryTest(const LogEntryTest &entry) {
         index_ = entry.index_;
@@ -15,6 +19,14 @@ struct LogEntryTest {
     int64_t term_;
 };
 
+bool g_flag = false;
+void thread_func(LogManagerMutexImpl<LogEntryTest> &log_manager) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    g_flag = true;
+    LogEntryTest log_entry{123, 456};
+    log_manager.Push(log_entry);
+}
+
 TEST_CASE("LogManagerTest") {
     using namespace raftcpp;
 
@@ -24,4 +36,27 @@ TEST_CASE("LogManagerTest") {
     LogEntryTest res = log_manager.Pop();
     REQUIRE_EQ(log_entry.index_, res.index_);
     REQUIRE_EQ(log_entry.term_, res.term_);
+}
+
+TEST_CASE("LogManagerEmptyTest, NonBlocking") {
+    using namespace raftcpp;
+
+    LogManagerMutexImpl<LogEntryTest> log_manager;
+    LogEntryTest log_entry;
+    bool res = log_manager.Pop(log_entry);
+    REQUIRE_EQ(res, false);
+}
+
+TEST_CASE("LogManagerTest, Blocking") {
+    using namespace raftcpp;
+
+    LogManagerMutexImpl<LogEntryTest> log_manager;
+    REQUIRE_EQ(g_flag, false);
+    std::thread th(thread_func, std::ref(log_manager));
+    LogEntryTest res;
+    res = log_manager.Pop();
+    REQUIRE_EQ(g_flag, true);
+    REQUIRE_EQ(res.index_, 123);
+    REQUIRE_EQ(res.term_, 456);
+    th.join();
 }
