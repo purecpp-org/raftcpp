4e736865a6f358c353bb8b2773b52676b7d191cb
Qing Wang <kingchin1218@gmail.com>
2021-06-29 20:38:02 +0800

    Fix

--- src/log_manager/leader_log_manager.h
+++ src/log_manager/leader_log_manager.h
@@ -8,6 +8,9 @@
 #include "common/constants.h"
 #include "log_manager/blocking_queue_interface.h"
 #include "log_manager/blocking_queue_mutex_impl.h"
+#include "log_manager/log_entry.h"
+#include "rpc/common.h"
+#include "rest_rpc.hpp"
 
 namespace raftcpp {
 
