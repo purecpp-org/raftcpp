f284de5e3fecfa1930c9e0b8c6f87325e3507796
kairbon <45480357+kairbon@users.noreply.github.com>
2021-07-06 15:09:22 +0800

    Implamentation of toString and << in LogEntry (#164)
    * add<<
    
    * fix
    
    * fix
    
    Co-authored-by: Kai Wang <kaiwang@B-V0JGMD6R-2318.local>

--- src/log_manager/log_entry.h
+++ src/log_manager/log_entry.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <sstream>
+
 #include "common/id.h"
 
 namespace raftcpp {
@@ -13,6 +15,17 @@ struct LogEntry final {
     uint64_t log_index;
 
     std::string data;
+
+    std::string toString() {
+        return "{\n\ttermId: " + std::to_string(term_id.getTerm()) + "," +
+               "\n\tlogIndex: " + std::to_string(log_index) + "," +
+               "\n\tdata: " + data.substr(0, 100) + " \n}";
+    };
+
+    std::stringstream &operator<<(std::stringstream &ss) {
+        ss.str(this->toString());
+        return ss;
+    }
 
     MSGPACK_DEFINE(term_id, log_index, data);
 };
