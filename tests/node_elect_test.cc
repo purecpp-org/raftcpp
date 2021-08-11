52c3f7d07bf06fb93e51a68c68c2f7aa7340b9c7
carl <289012365@qq.com>
2021-08-11 16:45:58 +0800

    Increase thread sleep time in the test

--- tests/node_elect_test.cc
+++ tests/node_elect_test.cc
@@ -139,7 +139,7 @@ TEST(NodeElectTest, TestNodeElect) {
     }
 
     // wait for their initialization
-    std::this_thread::sleep_for(std::chrono::seconds(3));
+    std::this_thread::sleep_for(std::chrono::seconds(5));
 
     nodeStateFollower.clear();
     nodeStateLeader.clear();
